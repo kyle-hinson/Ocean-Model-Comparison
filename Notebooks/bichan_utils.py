9789b80dc72dfe3deeb5374136555f6f629e3b83
kyle-hinson <101892482+kyle-hinson@users.noreply.github.com>
2025-10-09 16:45:39 -0400

    Add files via upload

--- Notebooks/bichan_utils.py
+++ Notebooks/bichan_utils.py
@@ -85,3 +85,185 @@ def add_derivatives(ds, grid, q='salt'):
     
     return ds
 
+
+def pl33tn(x, dt=1.0, T=33.0, mode="valid", t=None):
+    """
+    Computes low-passed series from `x` using pl33 filter, with optional
+    sample interval `dt` (hours) and filter half-amplitude period T (hours)
+    as input for non-hourly series.
+
+    The PL33 filter is described on p. 21, Rosenfeld (1983), WHOI
+    Technical Report 85-35.  Filter half amplitude period = 33 hrs.,
+    half power period = 38 hrs.  The time series x is folded over
+    and cosine tapered at each end to return a filtered time series
+    xf of the same length.  Assumes length of x greater than 67.
+
+    Can input a DataArray and use dask-supported for lazy execution. In that
+    use case, dt is ignored and calculated from the input DataArray.
+    cf-xarray is also required.
+
+    Examples
+    --------
+    >>> from oceans.filters import pl33tn
+    >>> import matplotlib.pyplot as plt
+    >>> t = np.arange(500)  # Time in hours.
+    >>> x = 2.5 * np.sin(2 * np.pi * t / 12.42)
+    >>> x += 1.5 * np.sin(2 * np.pi * t / 12.0)
+    >>> x += 0.3 * np.random.randn(len(t))
+    >>> filtered_33 = pl33tn(x, dt=4.0)  # 33 hr filter
+    >>> filtered_33d3 = pl33tn(x, dt=4.0, T=72.0)  # 3 day filter
+    >>> fig, ax = plt.subplots()
+    >>> (l1,) = ax.plot(t, x, label="original")
+    >>> pad = [np.nan] * 8
+    >>> (l2,) = ax.plot(t, np.r_[pad, filtered_33, pad], label="33 hours")
+    >>> pad = [np.nan] * 17
+    >>> (l3,) = ax.plot(t, np.r_[pad, filtered_33d3, pad], label="3 days")
+    >>> legend = ax.legend()
+
+
+    """
+
+    # import cf_xarray  # noqa: F401
+    # import pandas as pd
+    # import xarray as xr
+
+    if isinstance(x, xr.Dataset | pd.DataFrame):
+        raise TypeError("Input a DataArray not a Dataset, or a Series not a DataFrame.")
+
+    if isinstance(x, pd.Series) and not isinstance(
+        x.index,
+        pd.core.indexes.datetimes.DatetimeIndex,
+    ):
+        raise TypeError("Input Series needs to have parsed datetime indices.")
+
+    # find dt in units of hours
+    if isinstance(x, xr.DataArray):
+        dt = (x.cf["T"][1] - x.cf["T"][0]) / np.timedelta64(
+            3_600_000_000_000,
+        )
+    elif isinstance(x, pd.Series):
+        dt = (x.index[1] - x.index[0]) / pd.Timedelta("1H")
+
+    pl33 = np.array(
+        [
+            -0.00027,
+            -0.00114,
+            -0.00211,
+            -0.00317,
+            -0.00427,
+            -0.00537,
+            -0.00641,
+            -0.00735,
+            -0.00811,
+            -0.00864,
+            -0.00887,
+            -0.00872,
+            -0.00816,
+            -0.00714,
+            -0.00560,
+            -0.00355,
+            -0.00097,
+            +0.00213,
+            +0.00574,
+            +0.00980,
+            +0.01425,
+            +0.01902,
+            +0.02400,
+            +0.02911,
+            +0.03423,
+            +0.03923,
+            +0.04399,
+            +0.04842,
+            +0.05237,
+            +0.05576,
+            +0.05850,
+            +0.06051,
+            +0.06174,
+            +0.06215,
+            +0.06174,
+            +0.06051,
+            +0.05850,
+            +0.05576,
+            +0.05237,
+            +0.04842,
+            +0.04399,
+            +0.03923,
+            +0.03423,
+            +0.02911,
+            +0.02400,
+            +0.01902,
+            +0.01425,
+            +0.00980,
+            +0.00574,
+            +0.00213,
+            -0.00097,
+            -0.00355,
+            -0.00560,
+            -0.00714,
+            -0.00816,
+            -0.00872,
+            -0.00887,
+            -0.00864,
+            -0.00811,
+            -0.00735,
+            -0.00641,
+            -0.00537,
+            -0.00427,
+            -0.00317,
+            -0.00211,
+            -0.00114,
+            -0.00027,
+        ],
+    )
+
+    _dt = np.linspace(-33, 33, 67)
+
+    dt = float(dt) * (33.0 / T)
+
+    filter_time = np.arange(0.0, 33.0, dt, dtype="d")
+    Nt = len(filter_time)
+    filter_time = np.hstack((-filter_time[-1:0:-1], filter_time))
+
+    pl33 = np.interp(filter_time, _dt, pl33)
+    pl33 /= pl33.sum()
+
+    if isinstance(x, xr.DataArray):
+        x = x.interpolate_na(dim=x.cf["T"].name)
+
+        weight = xr.DataArray(pl33, dims=["window"])
+        xf = (
+            x.rolling({x.cf["T"].name: len(pl33)}, center=True)
+            .construct({x.cf["T"].name: "window"})
+            .dot(weight, dims="window")
+        )
+        # update attrs
+        attrs = {
+            key: f"{value}, filtered"
+            for key, value in x.attrs.items()
+            if key != "units"
+        }
+        xf.attrs = attrs
+
+    elif isinstance(x, pd.Series):
+        xf = x.to_frame().apply(np.convolve, v=pl33, mode=mode)
+
+        # nan out edges which are not good values anyway
+        if mode == "same":
+            xf[: Nt - 1] = np.nan
+            xf[-Nt + 2 :] = np.nan
+
+    else:  # use numpy
+        xf = np.convolve(x, pl33, mode=mode)
+
+        # times to match xf
+        if t is not None:
+            # Nt = len(filter_time)
+            tf = t[Nt - 1 : -Nt + 1]
+            return xf, tf
+
+        # nan out edges which are not good values anyway
+        if mode == "same":
+            xf[: Nt - 1] = np.nan
+            xf[-Nt + 2 :] = np.nan
+
+    return xf
